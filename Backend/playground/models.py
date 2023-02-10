a6932e1d0442a8ec8a5611e05e83e49a7d4d911b
Neil <77575319+Armweak5104@users.noreply.github.com>
2023-02-10 08:56:16 +0800

    Update models.py

--- Backend/playground/models.py
+++ Backend/playground/models.py
@@ -6,7 +6,10 @@ class User(models.Model):
     phone_num  = models.CharField(max_length = 200, null = False, blank = False)
     password  = models.TextField()
     donor = models.BooleanField()
-    inventory = models.TextField(blank = True)
+    need_inventory = False
+    if(donor):
+        need_inventory  = True
+    inventory = models.TextField(blank = need_inventory)
     address = models.TextField()
     
     def __str__(self):
